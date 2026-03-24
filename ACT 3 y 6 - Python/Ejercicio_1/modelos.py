5fcc4a71ea995cdb9b796d89b80835a04fc1675a
SEBASTIAN GUTIERREZ GUAYACUNDO <sgutierrez78@unisalle.edu.co>
2026-03-23 22:16:53 -0500

    Implementar Orden de Compra y Stock

--- ACT 3 y 6 - Python/Ejercicio_1/modelos.py
+++ ACT 3 y 6 - Python/Ejercicio_1/modelos.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import List, Dict, Any
 
 class Producto:
     def __init__(self, codigo: str, descripcion: str, precio: float, cantidad: int):
@@ -14,3 +15,17 @@ class MetodoPago(ABC):
     @abstractmethod
     def procesar(self, monto: float) -> None:
         pass
+
+class OrdenCompra:
+    def __init__(self, cliente: str):
+        self.cliente = cliente
+        self.items: List[Dict[str, Any]] = []
+        self.confirmada = False
+
+    def agregar_item(self, producto: Producto, cantidad: int):
+        if producto.cantidad >= cantidad:
+            self.items.append({"producto": producto, "cantidad": cantidad})
+            producto.cantidad -= cantidad # Actualiza disponibilidad según el taller [cite: 29]
+            print(f"Agregado: {cantidad} unidades de {producto.descripcion}")
+        else:
+            print(f"Error: No hay stock suficiente de {producto.descripcion}")
